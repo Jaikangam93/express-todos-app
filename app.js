90b9ae04a5129728b12e8b65632e1720f673e3ee
Jaikangam Malangmei <jaikangam12@gmail.com>
2019-10-12 14:51:09 +0530

    fetch data from mongodb

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const port = 3000;
 //Init app
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const url = 'mongodb://localhost:27017/todoapp';
+
 //Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false }));
@@ -15,11 +19,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname,'views' ));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res, next) => {
-    res.render('index');
+// Connect to mongodb
+MongoClient.connect(url, (err, database) => {
+  console.log('MongoDB Connected...');
+  if(err) throw err;
+
+  db = database;
+  Todos = db.collection('todos');
+
+  app.listen(port, () => {
+    console.log('Server running on port '+port);
+  });
 });
 
 
-app.listen(port, () => {
-    console.log('Server running on port'+port);
+app.get('/', (req, res, next) => {
+  Todos.find({}).toArray((err, todos) => {
+    if(err){
+      return console.log(err);
+    }
+    res.render('index',{
+      todos: todos
+    });
+  });
 });
